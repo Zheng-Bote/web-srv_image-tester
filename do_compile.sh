30b82a809f113adb8b908096f72f274fff5610b9
ZHENG Robert <78920748+Zheng-Bote@users.noreply.github.com>
2025-02-18 18:48:48 +0100

    Update do_compile.sh

--- do_compile.sh
+++ do_compile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 cd build
-cmake -B . -S ../src -DCMAKE_BUILD_TYPE=Release
+cmake -B . -S ../src -DCMAKE_BUILD_TYPE=Debug
 cmake --build . --parallel 6
 
 ./crow_web static/crow_web.ini
